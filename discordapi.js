c66570a758674b684812a69fd7f44af0fa4c6fbf
ThanushanKanes <82912548+ThanushanKanes@users.noreply.github.com>
2021-04-30 03:19:32 +0100

    commit
    commit

--- discordapi.js
+++ discordapi.js
@@ -1,5 +1,11 @@
+//checking if client is active
+console.log('The Client is ready!')
+
+//imports discord.js module
 const Discord = require ('discord.js')
+//opening a new discord client
 const client = new Discord.Client()
+
 const querystring = require('querystring')
 const fetch = require('node-fetch')
 
@@ -7,39 +13,41 @@ const fetch = require('node-fetch')
 prefix = '!'
 
 //connecting to discord
-client.login('ODMxMjA4NTQ1NDExMTM3NTk2.YHR5SA.mCZxP7SjvAx7vRy6aJ945ISkBac')
+client.login('ODMxMjA4NTQ1NDExMTM3NTk2.YHR5SA.RmPClDzt1qx5tsOt3U4qO4D11Qs')
 
 
-
-//checking if client is active
-console.log('The Client is ready!')
 {
-
+    //the event will trigger multiple time after loggin in
     client.on('message', async message => 
     {
 
-
+        //if prefix is found, set up args, otherwise it isn't a command
         if(!message.content.startsWith(prefix))return; 
         const args = message.content.substring(prefix.length).split(" ")
         {
-
-            if (message.channel.id == '832322311037255700' && message.content.startsWith(prefix + 'define')) 
-            {		
+            //if prefix with define message in command   
+            if (message.content.startsWith(prefix + 'define')) 
+            {	
+                //using the stringfy function method on the object	
                 const searchString = querystring.stringify(
                 { 
                     term: args.slice(1).join(" ") 
                 })
-                        
+                
+                //if command doesn't meet the requirement 
+                //using messageEmbed to set border colour and descripiton
                 if (!args.slice(1).join(" ")) return message.channel.send(new MessageEmbed()
                 .setColor("GOLD")
                 .setDescription('You need to specify something you want to search the urban dictionary'))
-                        
+                 
+                //fetching the definition of the word from the API 
                 const { list } = await fetch(
                     `https://api.urbandictionary.com/v0/define?${searchString}`)
                     .then(response => response.json())
 
                 try 
                 {
+                    //setting out the command output
                     const [answer] = list
 
                     const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str)
@@ -56,9 +64,10 @@ console.log('The Client is ready!')
                 } 
                 catch (error) 
                 {
+                    //if no defintion in the command, give reply
                     console.log(error)
                     return message.channel.send(
-                    MessageEmbed()
+                    message.channel.send(embed)
                     .setColor("RED")
                     .setDescription(`No results were found for **${args.slice(1).join(" ")}**`))
                             
